916235dd15254413bfdc7752a9181b005306f82f
maeganskinnerblog <maeganskinnerblog@gmail.com>
2019-08-15 16:50:38 -0500

    first try

--- javascript-1/practice.js
+++ javascript-1/practice.js
@@ -2,73 +2,115 @@
 
 // Create a variable called 'myName' that's value is your name
 
-// Code here
+var myName="Maegan"
 
 //////////////////PROBLEM 2////////////////////
 
 // Create a variable called 'faveNum' that's value is your favorite number
 
-//Code here
+var faveNum=20
 
 //////////////////PROBLEM 3////////////////////
 
 // Create a variable called 'lovesCode' and set it to true
 
-//Code here
+var lovesCode=true
 
 //////////////////PROBLEM 4////////////////////
 
 // Create a function called 'sum' that takes in two parameters, num1 and num2, and returns the sum of those two parameters.
 
-//Code here
+function sum(num1, num2){
+	return num1 + num2
+}
 
 //////////////////PROBLEM 5////////////////////
 
-// Create a function called 'lovesCodeChecker' that takes in a single parameter called x. Check to see if x is equal to true. If it is, return true as a boolean. If x does not equal true, return false as a boolean
+// Create a function called 'lovesCodeChecker' that takes in a single parameter called x. Check to see if x is equal to true. 
+// If it is, return true as a boolean. If x does not equal true, return false as a boolean
 
-//Code here
+function lovesCodeChecker(x){
+	lovesCodeChecker=x
+	if (x==true){
+		return true
+	} else {
+		return false
+	}
+}
+
+
 
 //////////////////PROBLEM 6////////////////////
 
-// Create a function called 'oddChecker' that takes in one parameter, num. Check to see if the number is even or odd. If it is odd, return 'the number is odd' or return 'the number is even' if it is even.
+// Create a function called 'oddChecker' that takes in one parameter, num. Check to see if the number is even or odd. 
+// If it is odd, return 'the number is odd' or return 'the number is even' if it is even.
 
-//Code here
+function oddChecker(num){
+	// num = i
+	// if (i = [2,4,6,8,10]){
+	//	return "the number is true"
+	//}
+	//else if (i = [1,3,5,7,9]){
+	//	return "the number is odd"
+	//}
+}
 
 //////////////////PROBLEM 7////////////////////
 
-// Create a function called 'iLove' that takes in two parameters, name which will be a string  and love which will be a string of something you love. Have the function take the two parameters and retrun a string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Bryan loves skateboarding"
+// Create a function called 'iLove' that takes in two parameters, name which will be a string  
+// and love which will be a string of something you love. Have the function take the two parameters and retrun a 
+// string that says "NAMEPARAM loves LOVEPARAM" with the appropriate parameters in the string. e.g. "Bryan loves skateboarding"
 
-//Code Here
+function iLove(name, love){
+	name="Bryan";
+	love="Skateboarding";
+	return iLove.name + iLove + iLove.love
+	
+
+}
 
 //////////////////PROBLEM 8////////////////////
 
 // Create a copy of the faveColors array called 'colorCopy' using slice
-const faveColors = ['red', 'green', 'black']
+ const faveColors = ['red', 'green', 'black']
 
-//Code Here
+var colorCopy = faveColors.slice()
+
+
 
 //////////////////PROBLEM 9////////////////////
 
 // Add a fourth color to the end of the 'colorCopy' array using push
 
-//Code Here
+colorCopy.push('yellow')
 
 //////////////////PROBLEM 10////////////////////
 
 // Create a new array called 'middleNums' from the numbers array that will capture only the middle numbers (2, 3, 4).
-const numbers = [1, 2, 3, 4, 5]
+ const numbers = [1, 2, 3, 4, 5]
 
-//Code Here
+ var middleNums = numbers.splice(2,3,4)
+
 
 //////////////////PROBLEM 11////////////////////
 
-// Create an object called 'me' that has these keys with the correct values: firstName, state, age, greeter. firstName will be your name as a string. state will be your current state or providence as a string. age will be your age as a number. greeter will be a method that returns the string 'Hello! My name is NAMEVALUE and I live in STATEVALUE' with the correct values
+// Create an object called 'me' that has these keys with the correct values: firstName, state, age, greeter. firstName 
+// will be your name as a string. state will be your current state or providence as a string. age will be your age as a number. 
+// greeter will be a method that returns the string 'Hello! My name is NAMEVALUE and I live in STATEVALUE' with the correct values
 
-//Code Here
+// me = {
+//	 firstName: "Maegan", 
+//	 state: "Texas", 
+//	 age: 25, 
+//	 greeter:  
+//	};
 
 //////////////////PROBLEM 12////////////////////
 
-// Create a function called 'bigOrSmall' that takes in one parameter, arr. Create a new array inside of bigOrSmall called 'answers'. Loop over the passed in arr param. If the number is GREATER than 100, push 'big' as a string to the answers array. If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. Return the answers array inside of the function
+// Create a function called 'bigOrSmall' that takes in one parameter, arr. Create a new array inside 
+// of bigOrSmall called 'answers'. Loop over the passed in arr param. If the number is GREATER than 100, push 'big' 
+// as a string to the answers array. If the number is LESS than or EQUAL to 100, push 'small' as a string to the answers array. 
+// Return the answers array inside of the function
 
 // Code here
 
